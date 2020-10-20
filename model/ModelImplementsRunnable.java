cfa9635e2a84053a023161f57ffea239d6d57f2d
Luis Bravo <luisclab@gmail.com>
2020-10-20 14:36:17 -0600

    Four commit - ModelImplementsRunnable ready - changed ModelExtendsThread

--- model/ModelImplementsRunnable.java
+++ model/ModelImplementsRunnable.java
@@ -4,7 +4,7 @@
  */
 package model;
 
-public class ModelExtendsThread extends Thread {
+public class ModelImplementsRunnable implements Runnable {
 
     private int id;
     private Model model;
@@ -15,7 +15,7 @@ public class ModelExtendsThread extends Thread {
         return (System.currentTimeMillis() - this.initialTime) / 1000;
     }
 
-    public ModelExtendsThread(int id, long initialTime, int size) {
+    public ModelImplementsRunnable(int id, long initialTime, int size) {
         this.id = id;
         this.model = new Model();
         this.initialTime = initialTime;
@@ -25,22 +25,24 @@ public class ModelExtendsThread extends Thread {
     public void run() {
         System.out.printf("[%d seconds] INIT OF THREAD %d\n", this.getTime(), this.id);
         //System.out.println("ARRAY " + id + ": " + Arrays.toString(this.array));
-        this.model.quicksort(this.array, true);
+        this.model.quicksort(this.array, false);
         //System.out.println("ARRAY " + id + " ORDENADO: " + Arrays.toString(this.array));
         System.out.printf("[%d seconds] FINISH OF THREAD %d\n", this.getTime(), this.id);
     }
 
-    public static void main(String[] args) throws InterruptedException {
-
-        System.out.println("Threads with priority = 5");
-        Thread t1 = new ModelExtendsThread(1, System.currentTimeMillis(), 100000000);
-        Thread t2 = new ModelExtendsThread(2, System.currentTimeMillis(), 1000000);
-        Thread t3 = new ModelExtendsThread(3, System.currentTimeMillis(), 10000000);
-
+    public static void main(String[] args) {
+        System.out.println("Threads with priority = 10");
+        Thread t1 = new Thread(new ModelImplementsRunnable(4, System.currentTimeMillis(), 1000000));
+        Thread t2 = new Thread(new ModelImplementsRunnable(5, System.currentTimeMillis(), 100000000));
+        Thread t3 = new Thread(new ModelImplementsRunnable(6, System.currentTimeMillis(), 1000));
+        
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+        t3.setPriority(Thread.MAX_PRIORITY);
+        
         t1.start();
         t2.start();
         t3.start();
-
     }
 
 } //CLASS KEY
